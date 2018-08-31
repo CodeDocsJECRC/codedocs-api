493caf8225af8e28e992627fa996679ff93e6000
maddhruv <dhruvjainpenny@gmail.com>
2018-08-31 06:44:22 +0530

    added members.alpha

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const membersAll = require('./data/members.json')
 exports.members = {
   all: membersAll
 }
+
+for (let alpha in membersAll) {
+  exports.members[alpha] = membersAll[alpha]
+}
--- test/members.js
+++ test/members.js
@@ -1,10 +1,46 @@
 const assert = require('assert')
 const members = require('..').members
 
-describe('members', function() {
-  describe('all', function() {
-    it('should have members', function() {
+describe('members', function () {
+  describe('all', function () {
+    it('should have members', function () {
       assert(Object.keys(members.all).length > 0)
+    })
+  })
+
+  describe('a member', function () {
+    it('should fetch a member', function () {
+      assert(members.maddhruv)
+    })
+  })
+
+  describe('not a member', function () {
+    it('should not show a member', function () {
+      assert(!members.alpha)
+    })
+  })
+
+  describe('has name', function () {
+    it('should have a name', function () {
+      assert(members.maddhruv.name === 'Dhruv Jain')
+    })
+  })
+
+  describe('has class', function () {
+    it('should have a class', function () {
+      assert(members.maddhruv.class === 'individual member')
+    })
+  })
+
+  describe('has skills', function () {
+    it('should have skills', function () {
+      assert(Array.isArray(members.maddhruv.skills))
+    })
+  })
+
+  describe('has avatar', function () {
+    it('should have an avatar', function () {
+      assert(members.maddhruv.avatar)
     })
   })
 })
